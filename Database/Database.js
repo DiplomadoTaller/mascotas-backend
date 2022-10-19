461dce13c5be9d2cb02e484d95391d327a984ddc
Carlos Ortega <carloslco246@gmail.com>
2022-10-18 20:29:01 -0500

    Se ajusta guardado de url de imagen

--- Database/Database.js
+++ Database/Database.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 /**
  * Creamos conexion a nuestra base de datos
  */
-const sequelize = new Sequelize('mascotas', 'postgres', 'postgres', {
+const sequelize = new Sequelize('mascotas', 'carlos', '@Unlock950409', {
     host: 'localhost',
     dialect:'postgres'
   });
